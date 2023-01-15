9a67edebf514f9292c98cb46b77d86061cccae83
Psi-Hate <dianaelzascudder@gmail.com>
2023-01-15 11:30:59 -0600

    update conda

--- conda/src/build.sh
+++ conda/src/build.sh
@@ -4,5 +4,5 @@ mkdir -p $PREFIX/bin/client/cores
 mkdir -p $PREFIX/Scripts
 mkdir -p $PREFIX/Scripts/client
 mkdir -p $PREFIX/Scripts/client/cores
-cp -r ./Z64Lib $PREFIX/bin/client/cores
-cp -r ./Z64Lib $PREFIX/Scripts/client/cores
+cp -r ./WindWaker $PREFIX/bin/client/cores
+cp -r ./WindWaker $PREFIX/Scripts/client/cores
--- conda/src/make_conda.sh
+++ conda/src/make_conda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CONDA_BLD_PATH=/mnt/d/modloader64.com
-export MYPKG_VERSION="2.1.1"
+export MYPKG_VERSION="1.0..0"
 export MYPKG_BUILD=1
 conda build .
